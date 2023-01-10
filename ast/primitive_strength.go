4a8ab272e4d0508fe4422a0c59f5ad52c6def160
Justin Dubs <jtdubs@gmail.com>
2023-01-10 12:14:17 -0500

    moar grammar

--- ast/primitive_strength.go
+++ ast/primitive_strength.go
@@ -1,1 +1,29 @@
 package ast
+
+import (
+	"fmt"
+
+	"github.com/jtdubs/go-nom"
+)
+
+type PulldownStrength struct {
+	nom.Span[rune]
+	ZeroT, OneT nom.Span[rune]
+	Zero        *Strength0
+	One         *Strength1
+}
+
+func (u *PulldownStrength) String() string {
+	return fmt.Sprintf("PulldownStrength(%v, %v)", u.Zero, u.One)
+}
+
+type PullupStrength struct {
+	nom.Span[rune]
+	ZeroT, OneT nom.Span[rune]
+	Zero        *Strength0
+	One         *Strength1
+}
+
+func (u *PullupStrength) String() string {
+	return fmt.Sprintf("PullupStrength(%v, %v)", u.Zero, u.One)
+}
--- grammar/primitive_strength.go
+++ grammar/primitive_strength.go
@@ -1,1 +1,66 @@
 package grammar
+
+import (
+	"context"
+
+	"github.com/jtdubs/go-nom"
+	"github.com/jtdubs/go-nom/fn"
+	"github.com/jtdubs/go-nom/runes"
+	"github.com/jtdubs/go-svparser/ast"
+)
+
+//
+// A.3.2 Primitive strengths
+//
+
+/*
+ * pulldown_strength ::=
+ *   ( strength0 , strength1 )
+ *   | ( strength1 , strength0 )
+ *   | ( strength0 )
+ */
+func PulldownStrength(ctx context.Context, start nom.Cursor[rune]) (nom.Cursor[rune], *ast.PulldownStrength, error) {
+	res := &ast.PulldownStrength{}
+	return tBind(res, &res.Span,
+		parens(
+			fn.Alt(
+				fn.Seq(
+					bindSpan(&res.ZeroT, bindValue(&res.Zero, Strength0)),
+					fn.Discard(fn.Surrounded(Whitespace0, Whitespace0, runes.Rune(','))),
+					bindSpan(&res.OneT, bindValue(&res.One, Strength1))),
+				fn.Seq(
+					bindSpan(&res.OneT, bindValue(&res.One, Strength1)),
+					fn.Discard(fn.Surrounded(Whitespace0, Whitespace0, runes.Rune(','))),
+					bindSpan(&res.ZeroT, bindValue(&res.Zero, Strength0))),
+				fn.Seq(
+					bindSpan(&res.ZeroT, bindValue(&res.Zero, Strength0))),
+			),
+		),
+	)(ctx, start)
+}
+
+/*
+ * pullup_strength ::=
+ *   ( strength0 , strength1 )
+ *   | ( strength1 , strength0 )
+ *   | ( strength1 )
+ */
+func PullupStrength(ctx context.Context, start nom.Cursor[rune]) (nom.Cursor[rune], *ast.PullupStrength, error) {
+	res := &ast.PullupStrength{}
+	return tBind(res, &res.Span,
+		parens(
+			fn.Alt(
+				fn.Seq(
+					bindSpan(&res.ZeroT, bindValue(&res.Zero, Strength0)),
+					fn.Discard(fn.Surrounded(Whitespace0, Whitespace0, runes.Rune(','))),
+					bindSpan(&res.OneT, bindValue(&res.One, Strength1))),
+				fn.Seq(
+					bindSpan(&res.OneT, bindValue(&res.One, Strength1)),
+					fn.Discard(fn.Surrounded(Whitespace0, Whitespace0, runes.Rune(','))),
+					bindSpan(&res.ZeroT, bindValue(&res.Zero, Strength0))),
+				fn.Seq(
+					bindSpan(&res.ZeroT, bindValue(&res.One, Strength1))),
+			),
+		),
+	)(ctx, start)
+}
